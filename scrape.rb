f7be6e31377db2541d0337fc5d7a201ddffc704d
ryoto.maeda <ryoto.maeda@PC0194.local>
2017-02-02 17:00:29 +0900

    initial

---
+++ scrape.rb
@@ -0,0 +1,32 @@
+
+require 'open-uri'
+require 'nokogiri'
+
+def scrape(url)
+  charset = nil
+  html = open(url) do |f|
+    charset = f.charset
+    f.read
+  end
+
+  doc = Nokogiri::HTML.parse(html, nil, charset)
+
+  doc.css('p').each do |sample|
+    str = sample.to_s
+    lines = str.split(/<br>/)
+    if !lines.nil?
+      lines.each do |line|
+        matches = line.match(/http:\/\/.*flickr.*|http:\/\/.*flk.*/)
+        if !matches.nil?
+          match = matches[0]
+          p match.sub(/]/, "")
+        end
+      end
+    end
+  end
+end
+
+ARGV.each_with_index do |arg, i|
+  url = arg
+  scrape(url)
+end
